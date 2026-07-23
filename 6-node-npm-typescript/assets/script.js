82087cfaba3cd0fb24062c437bd1b20fbb0f9fa8
Ederson <edersonprogramadorfullstack@outlook.com>
2026-07-23 18:58:51 -0300

    configurando o script.js do date-fns

--- 6-node-npm-typescript/assets/script.js
+++ 6-node-npm-typescript/assets/script.js
@@ -1,7 +1,9 @@
-import { format } from 'date-fns'
-const dateFns = require('date-fns')
+let dateFns = require('date-fns')
 
 let dataOriginal = new Date(2024, 1, 11)
 
 let data1 = dateFns.format(dataOriginal, "yyyy-MM-dd")
-let data2 = format(dataOriginal, "dd/MM/yyyy")
+let data2 = dateFns.format(dataOriginal, "dd/MM/yyyy")
+
+console.log(data1)
+console.log(data2)
